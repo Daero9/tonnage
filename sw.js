2878e6dc4f30e272421dd88546ab9a09d10718f3
Daero9 <nicoponcin30900@gmail.com>
2026-09-09 03:51:29 +0200

    Tonnup PUBLIQUE - 09/09/2026 03:51

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Tonnup — service worker : l'app marche hors ligne et se met à jour toute seule. */
-const CACHE = "tonnup-v10-20260909-0333";
+const CACHE = "tonnup-v10-20260909-0351";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./confidentialite.html", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png",
   "./img/exos/arnold-press.png",
   "./img/exos/australiennes.png",
