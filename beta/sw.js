7129cab5f5770d660cc9fa67ca025d630df055c8
Daero9 <nicoponcin30900@gmail.com>
2026-09-10 02:18:38 +0200

    Tonnup BÊTA - 10/09/2026 02:18

--- beta/sw.js
+++ beta/sw.js
@@ -1,5 +1,5 @@
 /* Tonnup — service worker : l'app marche hors ligne et se met à jour toute seule. */
-const CACHE = "tonnup-beta-v10-20260910-0213";
+const CACHE = "tonnup-beta-v10-20260910-0218";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./confidentialite.html", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png",
   "./img/exos/arnold-press.png",
   "./img/exos/australiennes.png",
